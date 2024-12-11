96ca2b32a934843da6823b2dec29b7b408970741
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 14:32:57 +0530

    Update for deployment

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const dbConnection = require("./Db/db");
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({ origin: ['http://localhost:3000', "*"], methods: ["GET", "POST"], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', process.env.CLIENT_URL], methods: ["GET", "POST"], credentials: true }));
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL || 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
@@ -33,7 +33,7 @@ app.use("/api/bookings/", require("./Routes/bookServicesRoute"));
 
 // Production setup
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
@@ -43,5 +43,5 @@ if (process.env.NODE_ENV === "production") {
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port} `);
+  console.log(`Server is running at port: ${port}`);
 });

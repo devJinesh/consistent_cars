e69d1d23e762afb84836e249b78df33d2d1ab8a9
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 15:41:44 +0530

    Bug Fix

--- Routes/bookServicesRoute.js
+++
@@ -1,9 +0,0 @@
-// routes/bookServicesRoute.js
-const express = require('express');
-const router = express.Router();
-const bookServiceController = require('../controllers/bookServiceController');
-
-router.get('/book-services', bookServiceController.getBookServices);
-router.post('/book-service', bookServiceController.bookService);
-
-module.exports = router;

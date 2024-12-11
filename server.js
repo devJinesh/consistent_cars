f215528d701b776a7921a9f02d9c2899eed22845
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 21:31:21 +0530

    Revert "Fix"
    This reverts commit 4ade923ebf0f20d9ec34de90c24fa35cac7cbaa7.

--- server.js
+++ server.js
@@ -8,31 +8,30 @@ const app = express();
 
 // Database connection
 const dbConnection = require("./Db/db");
-dbConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-dbConnection.once('open', () => {
-  console.log('Database connected');
-});
 
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://consistent-cars-frontend.onrender.com'],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(cors({ origin: ['http://localhost:3000', "*"], methods: ["GET", "POST"], credentials: true }));
 app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
 
 // Routes
-app.use("/api/cars", require("./Routes/carsRoutes"));
-app.use("/api/users", require("./Routes/usersRoutes"));
-app.use("/api/bookings", require("./Routes/bookingsRoute"));
+app.use("/api/cars/", require("./Routes/carsRoutes"));
+app.use("/booking/api/cars/", require("./Routes/carsRoutes"));
+app.use("/editcar/api/cars/", require("./Routes/carsRoutes"));
+app.use("/api/users/", require("./Routes/usersRoutes"));
+app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
+app.use("/api/bookings/", require("./Routes/bookingsRoute"));
 
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+  console.log(`Server is running at port: ${port} `);
 });

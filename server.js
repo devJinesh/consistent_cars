33a0802f977090412f766aea1d6ac60897620d27
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 21:31:05 +0530

    Revert "Update server.js"
    This reverts commit 25d75d71b85c7c6f1875238e599d983dd6bbeb08.

--- server.js
+++ server.js
@@ -23,25 +23,21 @@ app.use(function(req, res, next) {
 });
 
 // Routes
-app.use("/api/cars/", require("./Routes/carsRoutes"));
-app.use("/booking/api/cars/", require("./Routes/carsRoutes"));
-app.use("/editcar/api/cars/", require("./Routes/carsRoutes"));
-app.use("/api/users/", require("./Routes/usersRoutes"));
-app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
-app.use("/api/bookings/", require("./Routes/bookingsRoute"));
+app.use("/api/cars", require("./Routes/carsRoutes"));
+app.use("/api/users", require("./Routes/usersRoutes"));
+app.use("/api/bookings", require("./Routes/bookingsRoute"));
 
-// Serve static assets if in production
+// Serve static files for production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // All routes that are not handled by the API should be served by React
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port} `);
+  console.log(`Server is running at port: ${port}`);
 });

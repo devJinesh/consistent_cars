8c00fd5628b52c509079523f316c30e3498dc563
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 23:22:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Catch-all handler for any request that doesn't match an API route
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 

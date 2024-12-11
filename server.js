fb7792e7dea583cafe59d80201c9f44932400e81
ConsistentCars <consistent.cars@rediffmail.com>
2024-12-11 21:31:15 +0530

    Revert "Fix 2"
    This reverts commit 02b9abfb8b02c76c291367ba7bcaf658f4f900e2.

--- server.js
+++ server.js
@@ -30,16 +30,6 @@ app.use("/api/users/", require("./Routes/usersRoutes"));
 app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
 app.use("/api/bookings/", require("./Routes/bookingsRoute"));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // All routes that are not handled by the API should be served by React
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
